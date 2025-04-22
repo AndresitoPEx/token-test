b62bd651b620bf2c0bd459c0d4dad5a24fe34af9
AndresitopPEx <andresrt952@gmail.com>
2025-04-22 12:56:18 -0500

    v3

--- dbConfig.js
+++ dbConfig.js
@@ -3,16 +3,14 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-// Configuración para MySQL en Aiven con SSL
 const dbConfig = new ZOHOCRMSDK.DBBuilder()
   .host(process.env.DB_HOST)
-  .databaseName(process.env.DB_NAME || "zohooauth")
+  .databaseName(process.env.DB_NAME)
   .userName(process.env.DB_USER)
   .password(process.env.DB_PASSWORD)
-  .portNumber(process.env.DB_PORT || 3306)
+  .portNumber(process.env.DB_PORT)
   .tableName("oauthtoken")
-  // Configuración SSL para Aiven
-  .sslMode(true)
   .build();
+
 
 export default dbConfig;
